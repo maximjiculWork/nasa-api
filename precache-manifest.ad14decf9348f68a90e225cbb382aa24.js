6f8717a9bc578dcedc90ccbb02c747b293801bd1
maximjiculWork <57805416+maximjiculWork@users.noreply.github.com>
2020-08-07 15:57:41 +0300

    Updates

---
+++ precache-manifest.ad14decf9348f68a90e225cbb382aa24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacf5fa6b8b3b08b12a891387dbee918",
+    "url": "/nasa-api/index.html"
+  },
+  {
+    "revision": "35ac92c0a2bfcf0655bb",
+    "url": "/nasa-api/static/css/main.cec895c4.chunk.css"
+  },
+  {
+    "revision": "c9bde8c5a1852293b611",
+    "url": "/nasa-api/static/js/2.a8c47712.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-api/static/js/2.a8c47712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ac92c0a2bfcf0655bb",
+    "url": "/nasa-api/static/js/main.40d8f64d.chunk.js"
+  },
+  {
+    "revision": "62c0038090ab73b8aeb6",
+    "url": "/nasa-api/static/js/runtime-main.900dcfc7.js"
+  },
+  {
+    "revision": "82abe92f260fa390bbb5a91562b5573d",
+    "url": "/nasa-api/static/media/cosmos.82abe92f.jpg"
+  }
+]);
